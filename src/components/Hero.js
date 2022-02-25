adc9d6e4791243420340fbda181be7e55eb32baa
Catalina <catalinamcquade@gmail.com>
2022-02-25 17:41:37 -0500

    update: Modified JSX in Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,32 +1,22 @@
 const Hero = () => {
   return (
     <div className="main">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="entry-banner text-center">
-            <h1 id="hello">Hello, I'm</h1>
-            <h1 id="name">XXXX</h1>
-            <h1 id="person-description">
-              UI Developer. UX Desinger. Problem Solver.
-            </h1>
-          </div>
-        </div>
+      <div className="entry-banner text-center">
+        <h1 id="hello">Hello, I'm</h1>
+        <h1 id="name">XXXX</h1>
+        <h1 id="person-description">
+          UI Developer. UX Desinger. Problem Solver.
+        </h1>
+      </div>
 
-        <div className="row text-center">
-          <div className="banner-buttons">
-            <div className="col-sm-1 col-md-1 col-sm-offset-5 col-md-offset-5">
-              <a className="btn btn-default btn-border" href="#about">
-                Info
-              </a>
-            </div>
+      <div className="banner-buttons">
+        <a className="btn btn-default btn-border" href="#about">
+          Info
+        </a>
 
-            <div className="col-sm-1 col-md-1">
-              <a className="btn btn-default btn-border" href="#projects">
-                Portfolio
-              </a>
-            </div>
-          </div>
-        </div>
+        <a className="btn btn-default btn-border" href="#projects">
+          Portfolio
+        </a>
       </div>
     </div>
   );
